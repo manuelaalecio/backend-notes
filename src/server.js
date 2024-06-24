20e008d8fae86f21fc90352ec22dcd77c08df3f2
Manuela Alecio <manuelaalecio.dev@gmail.com>
2024-06-24 17:57:22 -0300

    feat: add users routes

--- src/server.js
+++ src/server.js
@@ -1,9 +1,10 @@
 const express = require("express");
-const app = express();
+const routes = require("./routes");
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
+const app = express();
+app.use(express.json());
+
+app.use(routes);
 
 const PORT = 3000;
 app.listen(PORT, () => {
