9f80d719361c9741280325a6fafbba89f10a2705
Manuela Alecio <manuelaalecio.dev@gmail.com>
2024-06-25 14:35:38 -0300

    feat: add update functionality

--- src/controller/UsersController.js
+++ src/controller/UsersController.js
@@ -1,3 +1,4 @@
+const { hash, compare } = require("bcryptjs");
 const AppError = require("../utils/AppError");
 const sqliteConnection = require("../database/sqlite");
 
@@ -15,12 +16,65 @@ class UsersController {
       throw new AppError("Este e-mail já está em uso.");
     }
 
+    const hashedPassword = await hash(password, 8);
+
     await database.run(
       "INSERT INTO users (name, email, password) VALUES (?, ?, ?)",
-      [name, email, password]
+      [name, email, hashedPassword]
     );
 
     return response.status(201).json();
+  }
+
+  async update(request, response) {
+    const { name, email, password, old_password } = request.body;
+    const { id } = request.params;
+
+    const database = await sqliteConnection();
+    const user = await database.get("SELECT * FROM users WHERE id = (?)", [id]);
+
+    const checkUserExists = await database.get(
+      "SELECT * FROM users WHERE id = (?)",
+      [id]
+    );
+
+    if (!user) {
+      throw new AppError("Usuário não encontrado");
+    }
+
+    const userWithUpdatedEmail = await database.get(
+      "SELECT * FROM users WHERE email = (?)",
+      [email]
+    );
+
+    if (userWithUpdatedEmail && userWithUpdatedEmail.id !== user.id) {
+      throw new AppError("Este e-mail já está em uso.");
+    }
+
+    user.name = name;
+    user.email = email;
+
+    if (password && !old_password) {
+      throw new AppError(
+        "Você precisa informar a senha antiga para definir a nova senha."
+      );
+    }
+
+    if (password && old_password) {
+      const checkOldPassword = await compare(old_password, user.password);
+      if (!checkOldPassword) {
+        throw new AppError("A senha antiga não confere");
+      }
+
+      user.password = await hash(password, 8);
+    }
+
+    await database.run(
+      "UPDATE users SET name = ?, email = ?, password = ?, updated_at = ? WHERE id = ?",
+      [user.name, user.email, user.password, new Date(), id]
+    );
+
+    return response.status(200).json();
   }
 }
 
--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -4,5 +4,6 @@ const usersRoutes = Router();
 
 const usersController = new UsersController();
 usersRoutes.post("/", usersController.create);
+usersRoutes.put("/:id", usersController.update);
 
 module.exports = usersRoutes;
