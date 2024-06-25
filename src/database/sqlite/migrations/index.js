bfe7806b68615ba4c8a1f248cc1acdf307865214
Manuela Alecio <manuelaalecio.dev@gmail.com>
2024-06-25 13:43:13 -0300

    feat: add users db with migrations

---
+++ src/database/sqlite/migrations/index.js
@@ -0,0 +1,11 @@
+const sqliteConnection = require("../../sqlite");
+const createUsers = require("./createUsers");
+
+async function migrationsRun() {
+  const schemas = [createUsers].join("");
+  sqliteConnection()
+    .then((db) => db.exec(schemas))
+    .catch((err) => console.error(err));
+}
+
+module.exports = migrationsRun;
--- src/server.js
+++ src/server.js
@@ -1,4 +1,5 @@
 require("express-async-errors");
+const migrationsRun = require("./database/sqlite/migrations");
 const AppError = require("./utils/AppError");
 const express = require("express");
 const routes = require("./routes");
@@ -7,6 +8,7 @@ const app = express();
 app.use(express.json());
 
 app.use(routes);
+migrationsRun();
 
 app.use((error, request, response, next) => {
   if (error instanceof AppError) {
