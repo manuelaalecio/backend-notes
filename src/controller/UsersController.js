94457962fb5a934856cfc89354523a37400896db
Manuela Alecio <manuelaalecio.dev@gmail.com>
2024-06-25 13:51:03 -0300

    feat: cheack if user exist funcition

--- src/controller/UsersController.js
+++ src/controller/UsersController.js
@@ -1,14 +1,20 @@
 const AppError = require("../utils/AppError");
+const sqliteConnection = require("../database/sqlite");
 
 class UsersController {
-  create(request, response) {
+  async create(request, response) {
     const { name, email, password } = request.body;
 
-    if (!name) {
-      throw new AppError("Name is required", 400);
-    }
+    const database = await sqliteConnection();
+    const checkUserExists = await database.get(
+      "SELECT * FROM users WHERE email = (?)",
+      [email]
+    );
 
-    response.status(201).json({ name, email, password });
+    if (checkUserExists) {
+      throw new AppError("Este e-mail já está em uso.");
+    }
+    return response.status(201).json();
   }
 }
 
