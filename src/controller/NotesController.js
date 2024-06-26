a3fbd044dae30ed19486d6a73535c450e318a525
Manuela Alecio <manuelaalecio.dev@gmail.com>
2024-06-26 10:29:07 -0300

    feat: add filter searching by tittle

--- src/controller/NotesController.js
+++ src/controller/NotesController.js
@@ -58,8 +58,10 @@ class NotesController {
   }
 
   async index(request, response) {
-    const { user_id } = request.query;
-    const notes = await knex("notes").where({ user_id }).orderBy("title");
+    const { title, user_id } = request.query;
+    const notes = await knex("notes").where({ user_id })
+    .whereLike("title", `%${title}%`)
+    .orderBy("title");
 
     return response.json({ notes });
   }
