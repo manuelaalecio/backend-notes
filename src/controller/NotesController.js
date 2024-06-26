0e61aa4b95799f9faebbc1567d727592bcecd486
Manuela Alecio <manuelaalecio.dev@gmail.com>
2024-06-26 12:59:16 -0300

    feat: add filter searching by tags

--- src/controller/NotesController.js
+++ src/controller/NotesController.js
@@ -58,10 +58,25 @@ class NotesController {
   }
 
   async index(request, response) {
-    const { title, user_id } = request.query;
-    const notes = await knex("notes").where({ user_id })
-    .whereLike("title", `%${title}%`)
-    .orderBy("title");
+    const { title, user_id, tags } = request.query;
+    let notes;
+
+    if (tags) {
+      const filterTags = tags.split(",").map((tag) => tag.trim());
+
+      notes = await knex("tags")
+        .select(["notes.id", "notes.title", "notes.user_id"])
+        .where("notes.user_id", user_id)
+        .whereLike("notes.title", `%${title}%`)
+        .whereIn("name", filterTags)
+        .innerJoin("notes", "notes.id", "tags.note_id")
+        .orderBy("notes.title");
+    } else {
+      notes = await knex("notes")
+        .where({ user_id })
+        .whereLike("title", `%${title}%`)
+        .orderBy("title");
+    }
 
     return response.json({ notes });
   }
