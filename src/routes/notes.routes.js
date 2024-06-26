a4ff8d7c15059ef4f343ffb33eeaa1fe6a6528f5
Manuela Alecio <manuelaalecio.dev@gmail.com>
2024-06-26 10:25:10 -0300

    feat: add index method in notes controller, that list notes

--- src/routes/notes.routes.js
+++ src/routes/notes.routes.js
@@ -3,6 +3,7 @@ const NotesController = require("../controller/NotesController");
 const notesRoutes = Router();
 
 const notesController = new NotesController();
+notesRoutes.get("/", notesController.index);
 notesRoutes.post("/:user_id", notesController.create);
 notesRoutes.get("/:id", notesController.show);
 notesRoutes.delete("/:id", notesController.delete);
