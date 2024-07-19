bb16dfc5c3afa605eb5182d3a41da77679a5293b
Manuela Alecio <manuelaalecio.dev@gmail.com>
2024-07-19 09:34:17 -0300

    feat: saving avatar img on database

--- src/configs/upload.js
+++ src/configs/upload.js
@@ -9,7 +9,7 @@ const MULTER = {
   storage: multer.diskStorage({
     destination: TMP_FOLDER,
     filename(request, file, callback) {
-      const fileHash = crypto.randomBytes(16).toString("hex");
+      const fileHash = crypto.randomBytes(10).toString("hex");
       const fileName = `${fileHash}-${file.originalname}`;
 
       return callback(null, fileName);
@@ -17,4 +17,4 @@ const MULTER = {
   }),
 };
 
-module.exports = { MULTER, UPLOADS_FOLDER, MULTER };
+module.exports = { TMP_FOLDER, MULTER, UPLOADS_FOLDER };
--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -3,12 +3,14 @@ const multer = require("multer");
 const uploadConfig = require("../configs/upload");
 
 const UsersController = require("../controller/UsersController");
+const UserAvatarController = require("../controller/UserAvatarController");
 const ensureAuthenticated = require("../middlewares/ensureAuthenticated");
 
 const usersRoutes = Router();
 const upload = multer(uploadConfig.MULTER);
 
 const usersController = new UsersController();
+const userAvatarController = new UserAvatarController();
 
 usersRoutes.post("/", usersController.create);
 usersRoutes.put("/", ensureAuthenticated, usersController.update);
@@ -16,10 +18,7 @@ usersRoutes.patch(
   "/avatar",
   ensureAuthenticated,
   upload.single("avatar"),
-  (request, response) => {
-    console.log(request.file.filename);
-    response.json();
-  }
+  userAvatarController.update
 );
 
 module.exports = usersRoutes;
